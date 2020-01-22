ac987a9f8d9f205501c6185f59c8ed56eb3c504e
Shane Weaver <shweaver@microsoft.com>
2020-01-22 11:38:43 -0800

    Adding MVVM sample

---
+++ MVVM_Demo/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace MVVM_Demo
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainViewModel ViewModel = new MainViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            DataContext = ViewModel;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            ViewModel.Load();
+        }
+    }
+}
